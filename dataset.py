6d6c7ea4fa39ae7a9c16c05a1ee100dd11756d08
Marek Paulik <marek.paulik@blindspot.ai>
2022-08-21 22:11:42 +0200

    finished loading

--- dataset.py
+++ dataset.py
@@ -1,9 +1,33 @@
 from PIL import Image
 import os
 import numpy as np
-import config
 from torch.utils.data import Dataset, DataLoader
 from torchvision.utils import save_image
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+
+
+both_transform = A.Compose(
+    [A.Resize(width=256, height=256),], additional_targets={"image0": "image"},
+)
+
+transform_only_input = A.Compose(
+    [
+        A.HorizontalFlip(p=0.5),
+        A.ColorJitter(p=0.2),
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        ToTensorV2(),
+    ]
+)
+
+transform_only_mask = A.Compose(
+    [
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        ToTensorV2(),
+    ]
+)
 
 class MapDataset(Dataset):
     def __init__(self, root_dir) -> None:
@@ -20,12 +44,12 @@ class MapDataset(Dataset):
         input_image = image[:, :600, :] # For MapDataset image is 1200pxs in width
         target_image = image[:, 600:, :]
 
-        augmentations = config.both_transform(image=input_image, image0=target_image)
+        augmentations = both_transform(image=input_image, image0=target_image)
         input_image, target_image = augmentations['image'], augmentations['image0']
 
 
-        input_image = config.transform_only_input(image=input_image)["image"]
-        target_image = config.transform_only_mask(image=target_image)["image"]
+        input_image = transform_only_input(image=input_image)["image"]
+        target_image = transform_only_mask(image=target_image)["image"]
 
         return input_image, target_image 
 
@@ -35,13 +59,14 @@ class MapDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset = MapDataset("data/maps/train")
-    loader = DataLoader(dataset, batch_size=1)
+    dataset = MapDataset("data/maps/maps/train")
+    loader = DataLoader(dataset, batch_size=1, shuffle=True)
 
     for x, y in loader:
         print(x.shape)
         save_image(x, "x.png")
         save_image(y, "y.png")
 
-
+        import sys
+        sys.exit()
 
